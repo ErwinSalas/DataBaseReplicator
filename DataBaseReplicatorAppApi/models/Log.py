788e27e22e5f72c95be3d15d529b70db86d22275
bryan-ruiz <bryanrb95@gmail.com>
2018-06-14 23:52:11 -0600

    logs model

---
+++ DataBaseReplicatorAppApi/models/Log.py
@@ -0,0 +1,9 @@
+
+class Log:
+    def __init__(self, name,connection):
+        self.name = name
+        self.connection = connection
+        print("database")
+
+    def getLogs(self):
+        print("logs")
